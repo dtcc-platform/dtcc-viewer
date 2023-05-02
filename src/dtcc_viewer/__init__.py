b31c713c123564c0255f8ddac266d0b843a6e95f
Dag Wästberg <dwastberg@gmail.com>
2023-05-02 16:05:09 +0200

    use new citymodel model API

--- src/dtcc_viewer/__init__.py
+++ src/dtcc_viewer/__init__.py
@@ -1,3 +1,5 @@
 from .citymodel import view as view_citymodel
 
+from dtcc_model import CityModel
 
+CityModel.add_processors(view_citymodel, "view")
--- src/dtcc_viewer/citymodel.py
+++ src/dtcc_viewer/citymodel.py
@@ -1,4 +1,3 @@
-
 import folium
 import tempfile
 import dtcc_io as io
@@ -8,28 +7,34 @@ from dtcc_io.bounds import bounds_center
 
 from .notebook_functions import is_notebook
 
-def show_folium_in_browser(m):
-    map_file = tempfile.NamedTemporaryFile(suffix=".html",delete=False)
+
+def _show_folium_in_browser(m):
+    map_file = tempfile.NamedTemporaryFile(suffix=".html", delete=False)
     map_file = Path(map_file.name)
     m.save(map_file)
     import webbrowser
+
     webbrowser.open(f"file:///{map_file}")
 
-def view(citymodel_pb, return_html=False, show_in_browser=False):
-    tmp_geojson = tempfile.NamedTemporaryFile(suffix=".geojson",delete=False)
+
+def view(cm, return_html=False, show_in_browser=False):
+    tmp_geojson = tempfile.NamedTemporaryFile(suffix=".geojson", delete=False)
     outpath = Path(tmp_geojson.name)
-    io.save_citymodel(citymodel_pb, outpath)
+    cm.save(outpath)
     bounds = io.citymodel.building_bounds(outpath)
     data_mid = bounds_center(bounds)
-    data_mid = [data_mid[1],data_mid[0]]
-    print(bounds)
-    print(data_mid)
-    m = folium.Map(location = data_mid, min_zoom=5, max_zoom=22, zoom_start=13)
-    
-    
+    data_mid = [data_mid[1], data_mid[0]]
+    # print(bounds)
+    # print(data_mid)
+    m = folium.Map(location=data_mid, min_zoom=5, max_zoom=22, zoom_start=13)
+
     with open(outpath, "r") as f:
         cm_geojson = f.read()
-    cm_layers =  folium.GeoJson(cm_geojson).add_child(folium.GeoJsonPopup(fields=['id','height','error'],aliases=['UUID','Height','Error']))  
+    cm_layers = folium.GeoJson(cm_geojson).add_child(
+        folium.GeoJsonPopup(
+            fields=["id", "height", "error"], aliases=["UUID", "Height", "Error"]
+        )
+    )
     m.add_child(cm_layers)
     tmp_geojson.close()
     outpath_dir = outpath.parent
@@ -42,13 +47,10 @@ def view(citymodel_pb, return_html=False, show_in_browser=False):
     if return_html:
         return m._repr_html_()
     if show_in_browser:
-        show_folium_in_browser(m)
-        
+        _show_folium_in_browser(m)
+
     else:
         if is_notebook():
             return m
         else:
-            show_folium_in_browser(m)
-        
-
-
+            _show_folium_in_browser(m)
