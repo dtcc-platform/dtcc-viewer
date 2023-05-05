425b87eb454edd5a832baa7e8887df8a1592aeb8
Jens Olsson <jens.olsson@chalmersindustriteknik.se>
2023-05-05 10:29:25 +0200

    Added camera to opengl implementation, updated dependencies list

--- sandbox/camera.py
+++ sandbox/camera.py
@@ -3,13 +3,50 @@ from math import sin, cos, radians
 
 
 
-class camera:
+class Camera:
+
+    def __init__(self):        
+        self.camera_pos = Vector3([0.0, 10.0, 3.0])
+        self.camera_front = Vector3([0.0, 0.0, -1.0])
+        self.camera_up = Vector3([0.0, 0.0, 1.0])
+        self.camera_right = Vector3([1.0, 0.0, 0.0]) 
+
+        self.mouse_sensitivity = 0.25
+        self.jaw = -90
+        self.pitch = 0
+
+    def get_view_matrix(self):
+        return matrix44.create_look_at(self.camera_pos, self.camera_pos + self.camera_front, self.camera_up)
+
+    def process_mouse_movement(self, xoffset, yoffset, constrain_pitch = True):
+        xoffset *= self.mouse_sensitivity
+        yoffset *= self.mouse_sensitivity
+
+        self.jaw += xoffset
+        self.pitch += yoffset
+
+        if constrain_pitch:
+            if self.pitch > 45:
+                self.pitch = 45
+            if self.pitch < -45:
+                self.pitch = -45    
+
+        self.update_camera_verctors()
 
 
-    def __inint__(self):
+    def update_camera_verctors(self):
+        front = Vector3([0.0, 0.0, 0.0])
+        front.x = cos(radians(self.jaw)) * cos(radians(self.pitch))
+        front.z = sin(radians(self.pitch))
+        front.y = sin(radians(self.jaw)) * cos(radians(self.pitch))
+
+        self.camera_front = vector.normalise(front)
+        self.camera_right = vector.normalise(vector3.cross(self.camera_front, Vector3([0.0, 0.0, 1.0])))
+        self.camera_up = vector.normalise(vector3.cross(self.camera_right, self.camera_front))
+
+
         
-        self.camera_pos = Vector3([0, 4, 3])
-        self.camera_front = Vector3([0,0,-1])
-        self.camera_up = Vector3([0,1,0])
-        self.camera_right = Vector3([1,0,0]) 
+        
+        
+
         
--- sandbox/opengl_9_draw_camera.py
+++ sandbox/opengl_9_draw_camera.py
@@ -7,14 +7,55 @@ from pprint import pp
 from enum import Enum
 import math
 from obj_loader import ObjLoader
+from camera import Camera
 
 class Projection(Enum):
     Pespective = 1
     Orthographic = 2
 
-window_w = 1200
-window_h = 1000
-proj = Projection.Orthographic
+camera = Camera()
+WIDTH = 1200
+HEIGHT = 1000
+last_x = WIDTH/2.0 
+last_y = HEIGHT/2.0
+first_mouse = True
+
+
+def key_input_callback(window, key, scancode, action, mode):
+    if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+
+
+def mouse_look_callback(window, xpos, ypos):
+    global last_x
+    global last_y
+    global first_mouse
+
+    # If the mouse leaves the window and enters the window again
+    # this if statement makes sure there is not a sudden jump since
+    # the last position could have been far from the xpos and ypos.  
+    if first_mouse:
+        last_x = xpos
+        last_y = ypos
+        first_mouse = False
+
+    xoffset = xpos - last_x
+    yoffset = last_y - ypos
+
+    last_x = xpos
+    last_y = ypos
+
+    camera.process_mouse_movement(xoffset, yoffset)
+
+# Called when the mouse enters the window
+def mouse_enter_callback(window, entered):
+    global first_mouse
+
+    if entered:
+        first_mouse = False
+    else:
+        first_mouse = True
+
 
 vertex_src = """
 # version 330 core
@@ -59,7 +100,7 @@ glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
 glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
 glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-window = glfw.create_window(window_w, window_h, "OpenGL Window", None, None)      # Create window
+window = glfw.create_window(WIDTH, HEIGHT, "OpenGL Window", None, None)      # Create window
 
 if not window:
     glfw.terminate()
@@ -69,33 +110,19 @@ glfw.set_window_pos(window, 400, 200)
 
 glfw.set_window_size_callback(window, window_resize)
 
+glfw.set_cursor_pos_callback(window, mouse_look_callback)
+
+glfw.set_key_callback(window, key_input_callback)
+
+#glfw.set_input_mode(window, glfw.CURSOR ,glfw.CURSOR_DISABLED)
+
+#glfw.set_cursor_enter_callback(window, mouse_enter_callback)
+
 # Calls can be made after the contex is made current
 glfw.make_context_current(window)
 
 #[face_indices, vert_coord, vert_color, vertices] = ObjLoader.load_model("./data/simple_city_2_color.obj")
 [face_indices, vert_coord, vert_color, vertices] = ObjLoader.load_model("./data/simple_city_dense.obj")
-
-"""
-vertices = [ -0.5, -0.5,  0.5, 1.0, 0.0, 1.0, 
-              0.5, -0.5,  0.5, 0.0, 1.0, 0.0, 
-              0.5,  0.5,  0.5, 0.0, 0.0, 1.0,
-             -0.5,  0.5,  0.5, 1.0, 1.0, 1.0,
-
-             -0.5, -0.5, -0.5, 1.0, 0.0, 1.0, 
-              0.5, -0.5, -0.5, 0.0, 1.0, 0.0, 
-              0.5,  0.5, -0.5, 0.0, 0.0, 1.0,
-             -0.5,  0.5, -0.5, 1.0, 1.0, 1.0]
-
-face_indices = [0, 1, 2, 2, 3, 0,
-                4, 5, 6, 6, 7, 4,
-                4, 5, 1, 1, 0, 4,
-                6, 7, 3, 3, 2, 6,
-                5, 6, 2, 2, 1, 5,
-                7, 4, 0, 0, 3, 7]
-
-vertices = np.array(vertices, dtype=np.float32)
-face_indices = np.array(face_indices, dtype=np.uint32)           
-"""
 
 # Generating VAO. Any subsequent vertex attribute calls will be stored in the VAO if it is bound.
 VAO = glGenVertexArrays(1)
@@ -124,10 +151,10 @@ glUseProgram(shader)
 glClearColor(0, 0.1, 0, 1)
 glEnable(GL_DEPTH_TEST)
 
-project = pyrr.matrix44.create_perspective_projection(45, window_w / window_h, 0.1, 100)
+project = pyrr.matrix44.create_perspective_projection(45, WIDTH / HEIGHT, 0.1, 100)
 translate = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
 #eye position, target position, up vector
-view = pyrr.matrix44.create_look_at(pyrr.Vector3([0, 30, 0]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 0, 1]))
+#view = pyrr.matrix44.create_look_at(pyrr.Vector3([0, 30, 0]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 0, 1]))
 
 model_loc = glGetUniformLocation(shader, "model")
 project_loc = glGetUniformLocation(shader, "project")
@@ -136,7 +163,7 @@ view_loc = glGetUniformLocation(shader, "view")
 # Only change when the window size changes
 glUniformMatrix4fv(project_loc, 1, GL_FALSE, project)
 glUniformMatrix4fv(model_loc, 1, GL_FALSE, translate)
-glUniformMatrix4fv(view_loc, 1, GL_FALSE, view) 
+#glUniformMatrix4fv(view_loc, 1, GL_FALSE, view) 
 
 
 # Main application loop
@@ -144,10 +171,12 @@ while not glfw.window_should_close(window):
     glfw.poll_events()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     
-    cam_x = math.sin(glfw.get_time()) * 30
-    cam_y = math.cos(glfw.get_time()) * 30 
+    #cam_x = math.sin(glfw.get_time()) * 30
+    #cam_y = math.cos(glfw.get_time()) * 30 
     
-    view = pyrr.matrix44.create_look_at(pyrr.Vector3([cam_x, cam_y, 5]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 0, 1]))
+    #view = pyrr.matrix44.create_look_at(pyrr.Vector3([cam_x, cam_y, 5]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 0, 1]))
+
+    view = camera.get_view_matrix()
 
     glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
 
