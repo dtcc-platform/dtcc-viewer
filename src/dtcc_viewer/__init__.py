fb81df6513c1a88464a6e2cf19e811a1e40ef4a7
Anders Logg <logg@chalmers.se>
2023-07-09 21:12:36 +0200

    Small fix for renamed CityModel --> City; works for now but should be renamed througout

--- src/dtcc_viewer/__init__.py
+++ src/dtcc_viewer/__init__.py
@@ -2,10 +2,10 @@ from .citymodel import view as view_citymodel
 from .mesh_opengl import view as view_mesh
 from .pointcloud_opengl import view as view_pointcloud
 
-from dtcc_model import CityModel
+from dtcc_model import City as CityModel # FIXME: Renamed to City
 from dtcc_model import Mesh
 from dtcc_model import PointCloud
 
-CityModel.add_processors(view_citymodel, "view")
-Mesh.add_processors(view_mesh, "view")
-PointCloud.add_processors(view_pointcloud, "view")
+CityModel.add_methods(view_citymodel, "view") # FIXME: --> view_city()
+Mesh.add_methods(view_mesh, "view")
+PointCloud.add_methods(view_pointcloud, "view")
