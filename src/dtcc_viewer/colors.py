ef1f5c61cca5881b89a7c01f807fb4809291524c
Jens Olsson <jens.olsson@chalmersindustriteknik.se>
2023-08-23 10:59:31 +0200

    Added more docstrings

--- src/dtcc_viewer/colors.py
+++ src/dtcc_viewer/colors.py
@@ -6,6 +6,23 @@ from .random_colors import get_random_colors
 def calc_colors_rainbow(
     values: Iterable[float], min: float = None, max: float = None
 ) -> List[List[float]]:
+    """
+    Calculate colors using a rainbow color map.
+
+    Parameters
+    ----------
+    values : iterable of float
+        The values to determine colors for.
+    min : float, optional
+        The minimum value for scaling, if not provided, the minimum value from `values` is used.
+    max : float, optional
+        The maximum value for scaling, if not provided, the maximum value from `values` is used.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     colors = []
     [min_value, max_value] = _get_min_max(values, min, max)
     for i in range(0, len(values)):
@@ -17,6 +34,23 @@ def calc_colors_rainbow(
 def calc_colors_warm(
     values: Iterable[float], min: float, max: float
 ) -> List[List[float]]:
+    """
+    Calculate colors using a warm color map.
+
+    Parameters
+    ----------
+    values : iterable of float
+        The values to determine colors for.
+    min : float
+        The minimum value for scaling.
+    max : float
+        The maximum value for scaling.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     colors = []
     [min_value, max_value] = _get_min_max(values, min, max)
     for i in range(0, len(values)):
@@ -28,6 +62,23 @@ def calc_colors_warm(
 def calc_colors_cold(
     values: Iterable[float], min: float, max: float
 ) -> List[List[float]]:
+    """
+    Calculate colors using a cold color map.
+
+    Parameters
+    ----------
+    values : iterable of float
+        The values to determine colors for.
+    min : float
+        The minimum value for scaling.
+    max : float
+        The maximum value for scaling.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     colors = []
     [min_value, max_value] = _get_min_max(values, min, max)
     for i in range(0, len(values)):
@@ -39,6 +90,23 @@ def calc_colors_cold(
 def calc_colors_mono(
     values: Iterable[float], min: float, max: float
 ) -> List[List[float]]:
+    """
+    Calculate colors using a monochromatic color map.
+
+    Parameters
+    ----------
+    values : iterable of float
+        The values to determine colors for.
+    min : float
+        The minimum value for scaling.
+    max : float
+        The maximum value for scaling.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     colors = []
     [min_value, max_value] = _get_min_max(values, min, max)
     for i in range(0, len(values)):
@@ -50,6 +118,23 @@ def calc_colors_mono(
 def calc_colors_arctic(
     values: Iterable[float], min: float, max: float
 ) -> List[List[float]]:
+    """
+    Calculate colors using an arctic color map.
+
+    Parameters
+    ----------
+    values : iterable of float
+        The values to determine colors for.
+    min : float
+        The minimum value for scaling.
+    max : float
+        The maximum value for scaling.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     colors = []
     for i in range(0, len(values)):
         fColor = [0.95, 0.95, 0.95, 1]
@@ -59,6 +144,21 @@ def calc_colors_arctic(
 
 # Calculate color blend for a range of values where some are excluded using a True-False mask
 def calc_colors_with_mask(values, mask) -> List[List[float]]:
+    """
+    Calculate colors for a range of values with a mask indicating exclusion.
+
+    Parameters
+    ----------
+    values : iterable of any
+        The values to determine colors for.
+    mask : iterable of bool
+        A mask indicating whether each value should be included in the coloring.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     colors = []
     min = np.min(values[mask])
     max = np.max(values[mask])
@@ -73,6 +173,19 @@ def calc_colors_with_mask(values, mask) -> List[List[float]]:
 
 # assign each distict value a random color
 def calc_colors_random(values: Iterable[Any]) -> List[List[float]]:
+    """
+    Assign random colors to distinct values.
+
+    Parameters
+    ----------
+    values : iterable of any
+        The values to determine colors for.
+
+    Returns
+    -------
+    List[List[float]]
+        List of color values for each input value in RGB format.
+    """
     unique_values = len(set(values))
     colors = get_random_colors(unique_values, return_int=False)
     colors = [c.append(1.0) for c in colors]
@@ -83,6 +196,23 @@ def calc_colors_random(values: Iterable[Any]) -> List[List[float]]:
 
 
 def _get_blended_color(min, max, value):
+    """
+    Calculate a blended color based on a value within a range.
+
+    Parameters
+    ----------
+    min : float
+        The minimum value of the range.
+    max : float
+        The maximum value of the range.
+    value : float
+        The value for which to calculate the color.
+
+    Returns
+    -------
+    List[float]
+        The calculated color value in RGB format.
+    """
     diff = max - min
     if (diff) <= 0:
         print(
@@ -128,21 +258,89 @@ def _get_blended_color(min, max, value):
 
 
 def _get_blended_color_mono(min, max, value):
+    """
+    Calculate a monochromatic color based on a value within a range.
+
+    Parameters
+    ----------
+    min : float
+        The minimum value of the range.
+    max : float
+        The maximum value of the range.
+    value : float
+        The value for which to calculate the color.
+
+    Returns
+    -------
+    List[float]
+        The calculated color value in RGB format.
+    """
     frac = _get_normalised_value_with_cap(min, max, value)
     return [frac, frac, frac, 1.0]
 
 
 def _get_blended_color_yellow_red(min, max, value):
+    """
+    Calculate a yellow-to-red color based on a value within a range.
+
+    Parameters
+    ----------
+    min : float
+        The minimum value of the range.
+    max : float
+        The maximum value of the range.
+    value : float
+        The value for which to calculate the color.
+
+    Returns
+    -------
+    List[float]
+        The calculated color value in RGB format.
+    """
     frac = _get_normalised_value_with_cap(min, max, value)
     return [1.0, (frac * 1.0), 0.0, 1.0]
 
 
 def _get_blended_color_cyan_blue(min, max, value):
+    """
+    Calculate a cyan-to-blue color based on a value within a range.
+
+    Parameters
+    ----------
+    min : float
+        The minimum value of the range.
+    max : float
+        The maximum value of the range.
+    value : float
+        The value for which to calculate the color.
+
+    Returns
+    -------
+    List[float]
+        The calculated color value in RGB format.
+    """
     frac = _get_normalised_value_with_cap(min, max, value)
     return [0.0, (frac * 1.0), 1.0, 1.0]
 
 
 def _get_min_max(values: Iterable[float], min: float = None, max: float = None):
+    """
+    Get the minimum and maximum values considering optional bounds.
+
+    Parameters
+    ----------
+    values : iterable of float
+        The values to determine the minimum and maximum from.
+    min : float, optional
+        The optional minimum value.
+    max : float, optional
+        The optional maximum value.
+
+    Returns
+    -------
+    Tuple[float, float]
+        The calculated minimum and maximum values.
+    """
     if min == None:
         min_value = np.min(values)
     else:
@@ -157,6 +355,23 @@ def _get_min_max(values: Iterable[float], min: float = None, max: float = None):
 
 
 def _get_normalised_value_with_cap(min, max, value):
+    """
+    Calculate a normalized value within a range with a cap.
+
+    Parameters
+    ----------
+    min : float
+        The minimum value of the range.
+    max : float
+        The maximum value of the range.
+    value : float
+        The value for which to calculate the normalized value.
+
+    Returns
+    -------
+    float
+        The calculated normalized value.
+    """
     diff = max - min
     if (diff) <= 0:
         print(
